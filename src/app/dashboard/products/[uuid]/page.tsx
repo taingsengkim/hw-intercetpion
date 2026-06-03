66b9a46348b6756682f6c6b7fc46bb11ac8c3507
unknown <mrrsaki0403@gmail.com>
2026-06-03 11:49:06 +0700

    Dynamic seo

--- src/app/dashboard/products/[uuid]/page.tsx
+++ src/app/dashboard/products/[uuid]/page.tsx
@@ -3,6 +3,32 @@ import React, { use } from "react";
 import { fetchDetail } from "../../@products/products/(..)products/[uuid]/page";
 import ProductDetailComponent from "@/components/products/ProductDetailComponent";
 
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: Promise<{ uuid: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const uuid = (await params)?.uuid;
+
+  // fetch post information
+  const post = await fetch(
+    `${process.env.BASE_ISHOP_API_URL}/products/${uuid}`,
+  ).then((res) => res.json());
+
+  return {
+    title: post?.name,
+    description: post?.description,
+    openGraph: {
+      images: post?.thumbnail,
+    },
+  };
+}
 export default async function ProductDetailPage({
   params,
 }: {
